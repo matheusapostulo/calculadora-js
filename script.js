be00392cd3ff3bdc5e7ba163e859696c9d6ad381
matheusapostulo <matheusapostulo10@gmail.com>
2023-01-09 21:28:51 -0300

    Adding JS file

---
+++ script.js
@@ -0,0 +1,10 @@
+// pegar todos os elementos  -> numero.onclick = chamaFuncao
+
+// uma funcao para fazer as operações
+//// se tiver uma operação já pode começar a exibir
+//// condicionais para evitar operações invalidas 
+
+// uma funcao para apagar tudo
+
+// um funcao para apagar um número por um                          
+
