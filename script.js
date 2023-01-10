3b757c8c818f509ad328d2de2850539f990d3215
matheusapostulo <matheusapostulo10@gmail.com>
2023-01-10 15:36:51 -0300

    Fazendo o botão on e off funcionar

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-// MUDAR A VISIBILITY DO ON/OFF
+// constante do resultado
 const resultado = document.querySelector('.resultado')
+
+// função para checar se a calculadora está ligada ou desligada
+function checarOnOff(){
+    situacao = window.getComputedStyle(resultado).visibility
+    console.log('A calculadora está', situacao)
+    situacao == 'visible' ? true : false
+}
+
+// MUDAR A VISIBILITY DO ON/OFF
+checarOnOff()
+
 const botao_on = document.querySelector('#on')
 const botao_off = document.querySelector('#off')
 
 // botão on
 botao_on.addEventListener('click',() => {
+    resultado.classList.remove('desligar')
     resultado.classList.add('ligar')
     console.log('Ligou')
+    checarOnOff()
 })
 
 // botão off
 botao_off.addEventListener('click', () =>{
     resultado.classList.add('desligar')
     console.log('Desligou')
+    checarOnOff()
 })
 
+// Temos que checar 
 
 
 
-// pegar todos os elementos  -> numero.onclick = chamaFuncao
-// Testando com o número 1
-const array_numeros = []
-
+// Clique dos botões
 function cliqueNumero(e){
     e.preventDefault()
-    array_numeros.push(1)
-    const resultado = document.querySelector('.resultado')
-    resultado.innerHTML = array_numeros
 
+    console.log(e.target.id)
 }
 
 const numero_1 = document.querySelector('#um')
